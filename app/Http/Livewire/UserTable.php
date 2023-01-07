0bc0b20dbf5a7372f56dd5f31685335579455f0f
Eric Barber <th@ericbarber.com>
2023-01-07 13:20:27 -0500

    First commit

---
+++ app/Http/Livewire/UserTable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use App\Models\User;
+
+class UserTable extends DataTableComponent
+{
+    protected $model = User::class;
+
+    public function configure(): void
+    {
+        $this->setPrimaryKey('id');
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make("Id", "id")
+                ->sortable(),
+            Column::make("Name", "name")
+                ->sortable(),
+            Column::make("Email", "email")
+                ->sortable(),
+            Column::make("Last Purchase", "latestPurchase.created_at"),
+        ];
+    }
+}
