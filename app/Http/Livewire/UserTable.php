56143f010819eabf11174030246f2b93ef05c253
Joe <joe@lowerrocklabs.com>
2023-01-08 21:47:01 +0000

    Interim Fix for Table

--- app/Http/Livewire/UserTable.php
+++ app/Http/Livewire/UserTable.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Database\Eloquent\Builder;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use App\Models\User;
 
 class UserTable extends DataTableComponent
 {
-    protected $model = User::class;
+    //protected $model = User::class;
 
     public function configure(): void
     {
@@ -24,7 +25,18 @@ class UserTable extends DataTableComponent
                 ->sortable(),
             Column::make("Email", "email")
                 ->sortable(),
-            Column::make("Last Purchase", "latestPurchase.created_at"),
+            Column::make('Last Purchase', 'id')
+            ->format(
+                fn ($value, $row, Column $column) =>  $row->latestPurchase->created_at
+            ),
         ];
+    }
+
+    /**
+     * @return Builder
+     */
+    public function builder(): Builder
+    {
+        return User::query()->with('latestPurchase');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -42,6 +42,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+
     public function latestPurchase()
     {
         return $this->hasOne(Purchase::class)->latestOfMany();
